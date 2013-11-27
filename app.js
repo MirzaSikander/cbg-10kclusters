93332abf4133d7ed69aee1c3d3822d5a7e83bb03
Autism <autism@dynamic2-155-201.usc.edu>
2013-11-26 23:29:11 -0800

    2d plots are also working

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ csv("shots-10000.csv")
  	complete_data.push(data);
  })
  .on("end", function(){
- 	listOfThingsToBeDone[0] = function(){ BucketsForOutliers(8, 60, -180, 180, 'velocity_angle'); }
+ 	/*listOfThingsToBeDone[0] = function(){ BucketsForOutliers(8, 60, -180, 180, 'velocity_angle'); }
  	listOfThingsToBeDone[1] = function(){ BucketsForOutliers(8, 10, -180, 180, 'velocity_angle'); }
 
  	listOfThingsToBeDone[2] = function(){ BucketsForOutliers(9, 10, 0, 30, 'dplayer_dist'); }
@@ -24,8 +24,17 @@ csv("shots-10000.csv")
  	listOfThingsToBeDone[7] = function(){ BucketsForOutliers(11, 2, -40, 40, 'dplayer_velocity_mag'); }
 
  	listOfThingsToBeDone[8] = function(){ BucketsForOutliers(12, 2, -10, 10, 'dplayer_velocity_angle'); }
- 	listOfThingsToBeDone[9] = function(){ BucketsForOutliers(12, 1, -10, 10, 'dplayer_velocity_angle'); }
-
+ 	listOfThingsToBeDone[9] = function(){ BucketsForOutliers(12, 1, -10, 10, 'dplayer_velocity_angle'); }*/
+/* 	listOfThingsToBeDone[0] = function(){ 
+ 		var f1 = {featureIndex: 11, bucketSize: 10, min: -40, max: 40, name: "dplayer_velocity_mag"};
+ 		var f2 = {featureIndex: 12, bucketSize: 2, min: -10, max: 10, name: "dplayer_velocity_angle"}
+ 		twoDimBucketsForOutliers(f1, f2);
+	};
+*/	listOfThingsToBeDone[0] = function(){ 
+ 		var f1 = {featureIndex: 9, bucketSize: 10, min: 0, max: 30, name: "dplayer_dist"};
+ 		var f2 = {featureIndex: 10, bucketSize: 60, min: -180, max: 180, name: "dplayer_angle"}
+ 		twoDimBucketsForOutliers(f1, f2);
+	};
  	listOfThingsToBeDone.shift()();
  })
  .parse();
@@ -132,6 +141,76 @@ function BucketsForOutliers(featureIndex, bucketSize, min, max, name ){
  	plot(probabilities, name+"_"+bucketSize);
 }
 
+function twoDimBucketsForOutliers(f1, f2){
+	f1.number_of_buckets = (f1.max- f1.min)/f1.bucketSize;
+ 	//adding two buckets for values less than min and anything greater than max
+ 	f1.number_of_buckets+=2;
+	f2.number_of_buckets = (f2.max- f2.min)/f2.bucketSize;
+ 	//adding two buckets for values less than min and anything greater than max
+ 	f2.number_of_buckets+=2;
+
+ 	//Creating 2d arrays
+ 	var table = new Array(f1.number_of_buckets);
+ 	for(var i = 0; i < f1.number_of_buckets; i++)
+ 		table[i] = new Array(f2.number_of_buckets);
+
+ 	var probabilities = new Array(f1.number_of_buckets);
+ 	for(var i = 0; i < f1.number_of_buckets; i++)
+ 		probabilities[i] = new Array(f2.number_of_buckets);
+
+
+ 	for(var i = 0; i < getTrainingSetSize(complete_data.length); i++){
+ 		var f1_value = complete_data[i][f1.featureIndex];
+ 		var f2_value = complete_data[i][f2.featureIndex];
+
+ 		var table_row = findBucketIndex(f1_value, f1.min, f1.bucketSize, f1.number_of_buckets);
+ 		var table_col = findBucketIndex(f2_value, f2.min, f2.bucketSize, f2.number_of_buckets);
+
+ 		if(typeof table[table_row][table_col] == 'undefined'){
+ 			table[table_row][table_col] = [];
+ 			table[table_row][table_col][0] = 1;	
+	 		if(complete_data[i][0] == 1)
+	 			table[table_row][table_col][1] = 1;
+	 		else
+	 			table[table_row][table_col][1] = 0;
+
+ 		}else{
+ 			table[table_row][table_col][0]++;
+	 		if(complete_data[i][0] == 1)
+	 			table[table_row][table_col][1]++;
+ 		}
+
+ 	}
+
+ 	for(var i = 0; i < f1.number_of_buckets; i++){
+	 	for(var j = 0; j < f2.number_of_buckets; j++){
+	 		if(typeof table[i][j] != "undefined")
+		 		probabilities[i][j] = table[i][j][1]/table[i][j][0];
+		}
+ 	}
+
+ 	var error = 0;
+ 	var testSetSize = getTestSetSize(complete_data.length);
+ 	for(var i = 5000; i < 5000 + testSetSize; i++){
+
+ 		var f1_value = complete_data[i][f1.featureIndex];
+ 		var f2_value = complete_data[i][f2.featureIndex];
+ 		//the loc of the bucket where the test entry would fall
+ 		var table_row = findBucketIndex(f1_value, f1.min, f1.bucketSize, f1.number_of_buckets);
+ 		var table_col = findBucketIndex(f2_value, f2.min, f2.bucketSize, f2.number_of_buckets);
+
+ 		var prob = probabilities[table_row][table_col];
+ 		if(typeof prob != 'undefined')
+	 		error += Math.pow( complete_data[i][0] - prob  , 2);
+	 	else
+	 		error += Math.pow( complete_data[i][0] - 0, 2);
+ 	}
+ 	console.log("Error rate for "+f1.name+" & "+f2.name+ 
+ 		"with bucketSize ("+f1.bucketSize+") & ("+f2.bucketSize+") respectively: "
+ 			+error/getTestSetSize(complete_data.length));
+ 	//
+ 	twoDimPlot(probabilities, f1.name+"_"+f2.name);
+}
 
 function plot(data, filename){
 	x = [];
@@ -144,6 +223,10 @@ function plot(data, filename){
 		"type": "bar"
 	};
 	sendData(barGraph, filename);
+}
+
+function twoDimPlot(data, filename){
+	sendData({"z": data, "type": "heatmap"}, filename);
 }
 
 function sendData(data, filename){
